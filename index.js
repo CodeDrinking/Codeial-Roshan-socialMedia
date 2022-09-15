ee3d3219d2e2a7edeca6ec7048176d25698dfd25
Roshan mestri <roshanmestri1999@gmail.com>
2022-09-15 13:07:52 +0530

    add user and post controller and there respective routes

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const port= 8000;
 
 
 //use express router
-app.use('/', require('./routes'))  // this for req for all routes , go to index.js of route
+app.use('/', require('./routes'))  // this for req for all routes , go to index.js of route 
+//this is entry point for routes
 
-app.set('view engine', 'ejs');
+//set ejs in server .
+app.set('view engine', 'ejs'); //key value pair
 app.set('views' , './views')
 
 app.listen(port, function(err){
